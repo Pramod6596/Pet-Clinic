ee64f204663d5a7e5d1bcc6cd4849afccb2603b4
Mac <mac@Pramod.local>
2022-01-22 12:12:44 +0530

    Initial Commit

---
+++ Pet-Clinic-Data/src/main/java/com/practice/multi_module/PetClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.practice.multi_module.PetClinic.model;
+
+public class Owner extends Person{
+}
---
+++ Pet-Clinic-Data/src/main/java/com/practice/multi_module/PetClinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.practice.multi_module.PetClinic.model;
+
+public class PetType
+{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
